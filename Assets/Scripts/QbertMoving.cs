4a725e0186974cd0bd09a8d927ca065413591faf
Davidrodvill <144342667+Davidrodvill@users.noreply.github.com>
2024-01-30 08:41:39 -0500

    ok

--- Assets/Scripts/QbertMoving.cs
+++ Assets/Scripts/QbertMoving.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class QbertMoving : MonoBehaviour
 {
+    public float speed = 5;
+    public Transform movePoint;
     // Start is called before the first frame update
     void Start()
     {
