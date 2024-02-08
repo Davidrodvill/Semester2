017fafb19d846f494199d1812051078c6325bde8
Davidrodvill <144342667+Davidrodvill@users.noreply.github.com>
2024-02-08 07:30:21 -0500

    movement done, y ay'

--- Assets/Scripts/QbertMoving.cs
+++ Assets/Scripts/QbertMoving.cs
@@ -5,32 +5,57 @@ using System;
 
 public class QbertMoving : MonoBehaviour
 {
-    public float speed = 5;
-    public GameObject movePointW, movePointA, movePointS, movePointD;
-    // Start is called before the first frame update
+    public float moveSpeed = 5f; // Speed of the jump
+    public Vector2 jumpHeight = new Vector2(1, 0.5f); // Height and length of the jump
+    public float moveCooldown = 0.5f; // Cooldown in seconds between moves
+
+    private bool canMove = true;
+    private Vector2 targetPosition;
+
     void Start()
     {
-        
+        targetPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (canMove)
         {
-            transform.position += movePointW.transform.position;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            transform.position -= movePointS.transform.position;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.position += movePointD.transform.position;
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            transform.position -= movePointA.transform.position;
+            CheckInput();
         }
     }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            StartCoroutine(MovePlayer(new Vector2(jumpHeight.x, jumpHeight.y)));
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(MovePlayer(new Vector2(-jumpHeight.x, -jumpHeight.y)));
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(MovePlayer(new Vector2(-jumpHeight.x, jumpHeight.y)));
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            StartCoroutine(MovePlayer(new Vector2(jumpHeight.x, -jumpHeight.y)));
+        }
+    }
+
+    IEnumerator MovePlayer(Vector2 direction)
+    {
+        canMove = false;
+        targetPosition += direction;
+        while ((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        yield return new WaitForSeconds(moveCooldown);
+        canMove = true;
+    }
+
 }
