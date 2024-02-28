f1a8d1bff02427f8d350cec3d1f5c4b3c7badf34
Davidrodvill <144342667+Davidrodvill@users.noreply.github.com>
2024-02-28 08:02:58 -0500

    i

--- Assets/NewScripts/NextLevels.cs
+++ Assets/NewScripts/NextLevels.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextLevels : MonoBehaviour
 {
+    BertController bertController;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,9 @@ public class NextLevels : MonoBehaviour
     void Update()
     {
         
+    }
+    public void level2()
+    {
+
     }
 }
