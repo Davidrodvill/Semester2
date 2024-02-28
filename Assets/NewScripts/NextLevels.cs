78a249ab534ec1111bc74018f2596bf8fddba7af
Davidrodvill <144342667+Davidrodvill@users.noreply.github.com>
2024-02-28 08:21:40 -0500

    n

--- Assets/NewScripts/NextLevels.cs
+++ Assets/NewScripts/NextLevels.cs
@@ -9,6 +9,7 @@ public class NextLevels : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //connecting to other script
         bertController = GetComponent<BertController>();
     }
 
